2ebfc925da2a8e0d839ea3cf0a664541ae6ef23c
elina-mns <ellie.mansurova@gmail.com>
2023-01-05 22:01:29 -0500

    Initial Commit

---
+++ Test-AssignmentUITests/Test_AssignmentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Test_AssignmentUITestsLaunchTests.swift
+//  Test-AssignmentUITests
+//
+//  Created by Elina Mansurova on 2023-01-05.
+//
+
+import XCTest
+
+final class Test_AssignmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
